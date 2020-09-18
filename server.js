5478d2becbf2c9c181931caa334ed0f5816f928e
chase104 <Chaniel123>
2020-09-18 18:19:27 +0200

    added body-parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 5000;
 const app = express();
