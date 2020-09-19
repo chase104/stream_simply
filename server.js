d124d544a0fb499b1077ac48b34bf469d223174d
chase104 <Chaniel123>
2020-09-19 14:49:36 +0200

    changed server.js

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-const bodyParser = require("body-parser");
+
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+if (process.env.NODE_ENV === 'production') {
+// Exprees will serve up production assets
+app.use(express.static('client/build'));
 
-// TODO setup mongooseDB
-// TODO connect to mongooseDB
-// TODO create user model
-// TODO add passport middleware
+// Express serve up index.html file if it doesn't recognize route
 
-//        --ROUTE LOGIC--
-
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-
-  // '*' will be used on any and every route
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+app.get('*', (req, res) => {
+ res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 }
 
-//        --ROUTES--
 
-//        --SERVER--
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.get('/getFeaturedMovie', (req, res) => {
+
+var options = {
+  method: 'GET',
+  url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup',
+  qs: {term: 'bojack', country: 'us'},
+  headers: {
+    'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+    'x-rapidapi-key': '12e1e06344mshbbfb57c2be00fdfp16c653jsnc7ee0c0aab1d',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	 if (!error && response.statusCode == 200) {
+     res.send(body)
+
+   }
+
+});
+  })
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
