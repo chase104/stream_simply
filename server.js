b50a04fe5e29b8437c562cf65a07ea32ef6e2941
chase104 <Chaniel123>
2020-09-21 17:33:49 +0200

    changed server.js for heroku

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const request = require('request');
 
+const path = require('path')
 
 const app = express();
 
+// Initialize Body Parser
+app.use(express.json())
+app.use(express.urlencoded( { extended: false }))
+
+// Set Static Folders
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 
-
+app.get('/', (req, res) => {
+  res.sendFild(path.join(__dirname, 'client', 'build', 'index.html'))
+})
+// app.use(express.static())
 app.get('/getFeaturedMovie', (req, res) => {
 
   var options = {
@@ -30,6 +40,6 @@ app.get('/getFeaturedMovie', (req, res) => {
 
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
