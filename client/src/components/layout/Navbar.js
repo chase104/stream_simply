966a9b97a27e7be0b4b39dda103719b467b48f14
chase104 <Chaniel123>
2020-10-09 18:52:47 +0200

    changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="nav-wrapper">
         <div className="logo-holder">
           <img src={Popcorn} className="popcorn-img left"/>
-          <Link className="brand-logo title-logo primary-brand" to="/">Stream Simplyy</Link>
+          <Link className="brand-logo title-logo primary-brand" to="/">Stream Simplyyy</Link>
         </div>
 
         <SignedOutLinks className="right " />
