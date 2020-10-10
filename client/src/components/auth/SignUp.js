edeb2499ea4c7d0f48925d0859a296c036c891bd
chase104 <Chaniel123>
2020-10-10 10:30:42 +0200

    updated for heroku ;

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -24,7 +24,7 @@ const SignUp = () => {
         },
         withCredentials: true,
         url: "/signup"
-      })
+      }).then((res) => {console.log(res);})
     } else {
       console.log('password error');
     }
