57fe9a700971aa0ad2252e34b10df49f8e6e682f
chase104 <Chaniel123>
2020-10-10 16:38:29 +0200

    added files

---
+++ db.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg')
+
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect(function(error) {
+  console.log("connected to db");
+});
+
+module.exports = client
---
+++ passport-config.js
@@ -0,0 +1,33 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const client = require('./db')
+
+module.exports = function(passport) {
+  passport.use(
+    new localStrategy({usernameField: 'email'}, async(email, password, done) => {
+      const user = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+      const exists = user.rows.length >= 1 ? true : false;
+      if (exists === false) {
+        return done(null, false, { message: "No user with that email"})
+      };
+      const foundUser = user.rows[0]
+      bcrypt.compare(password, foundUser.password, (err, result) => {
+        if (err) throw err;
+        if (result === true) {
+          return done(null, foundUser)
+        } else {
+          return done(null, false, { message: "Password incorrect"})
+        }
+      })
+    })
+  )
+  passport.serializeUser((user, cb) => {
+    cb(null, user.user_id);
+  })
+  passport.deserializeUser((id, cb) => {
+    client.query('SELECT * FROM users WHERE user_id = $1', [id], (err, result) => {
+      if (err) throw err
+      return cb(null, result.rows[0])
+    })
+  })
+}
